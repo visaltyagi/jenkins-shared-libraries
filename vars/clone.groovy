1b42d477ec65fade4d019519d51f322f4b6aec8e
VISAL TYAGI <107295081+visaltyagi@users.noreply.github.com>
2024-12-16 13:00:52 +0530

    Update clone.groovy

--- vars/clone.groovy
+++ vars/clone.groovy
@@ -1,5 +1,5 @@
 def call(String url, String branch){
   echo "This is cloning the code"
-  git url: "S{url}", branch: "${branch}
+  git url: "${url}", branch: "${branch}
   echo "Code Cloning Successful."
 }
