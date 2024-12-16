70475f426b9b2ba3e8583bae3ac11c142ec58969
VISAL TYAGI <107295081+visaltyagi@users.noreply.github.com>
2024-12-16 13:31:21 +0530

    Update docker_build.groovy

--- vars/docker_build.groovy
+++ vars/docker_build.groovy
@@ -1,4 +1,4 @@
 //Define Function
 def call (String ProjectName, String ImageTag, String DockerHubUser){
-  sh "docker build -t ${DockerHubUser}/${ProjectName}:#{ImageTag} ."
+  sh "docker build -t ${DockerHubUser}/${ProjectName}:${ImageTag} ."
 }
